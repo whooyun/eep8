8b61414af6eecd330ab18e9d51812b62195613bf
roy <1027329336@qq.com>
2018-02-09 22:43:18 +0800

    a

--- lifo/eep8common/src/main/java/com/lifopop/eep8/user/po/Eep8Test.java
+++ lifo/eep8common/src/main/java/com/lifopop/eep8/user/po/Eep8Test.java
@@ -11,11 +11,21 @@ public class Eep8Test implements Serializable {
 
     private String uId;
 
+    private String user_id;
+
     public String getuId() {
         return uId;
     }
 
     public void setuId(String uId) {
         this.uId = uId;
+    }
+
+    public String getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(String user_id) {
+        this.user_id = user_id;
     }
 }
